69c01f798f77f1c98175c6ce7362d6fbc7b8b340
Guangze Zu <gzu2018@gmail.com>
2020-07-11 21:31:26 -0500

    Updates

---
+++ precache-manifest.a4ac4956bbb8db2358cd061fec16c0a7.js
@@ -0,0 +1,30 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "40a167062c7eb3865675ff2337ba1f68",
+    "url": "/index.html"
+  },
+  {
+    "revision": "139899c89aacea836462",
+    "url": "/static/css/2.829c9cb5.chunk.css"
+  },
+  {
+    "revision": "525f2351c9d2129e901d",
+    "url": "/static/css/main.5facb584.chunk.css"
+  },
+  {
+    "revision": "139899c89aacea836462",
+    "url": "/static/js/2.7830cc93.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/static/js/2.7830cc93.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "525f2351c9d2129e901d",
+    "url": "/static/js/main.da0bcdb3.chunk.js"
+  },
+  {
+    "revision": "285ff90a2b78270b5cfe",
+    "url": "/static/js/runtime-main.1521cf29.js"
+  }
+]);
